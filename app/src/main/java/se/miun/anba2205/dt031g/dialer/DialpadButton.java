9ea889f59593630e735b4193ed085bab6b0b1911
Andreas Backman <anba2205@student.miun.se>
2024-01-25 14:53:28 +0100

    Task 2

--- app/src/main/java/se/miun/anba2205/dt031g/dialer/DialpadButton.java
+++ app/src/main/java/se/miun/anba2205/dt031g/dialer/DialpadButton.java
@@ -1,48 +1,23 @@
 package se.miun.anba2205.dt031g.dialer;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import se.miun.anba2205.dt031g.dialer.databinding.DialpadButtonBinding;
 
 public class DialpadButton extends ConstraintLayout {
 
-
-    private static final float SCALE_UP = 1.1f;
-    private static final float SCALE_DOWN = 1.0f;
-
+    private static final float SCALE_UP = 1.1f; // Used for button scaling
     private TextView title;
     private TextView message;
-
-    private Animation scaleUp, scaleDown;
-
-    private Button button;
-
-//    private OnClickListener listener;
-
-//    private DialpadClickListener listener;
-
 
     public DialpadButton(Context context) {
         super(context);
@@ -59,27 +34,32 @@ public class DialpadButton extends ConstraintLayout {
         initialize(context, attrs);
     }
 
+    // Initializes the view
     public void initialize(Context context, AttributeSet attrs) {
 
+        // Inflates the layout using binding
         DialpadButtonBinding binding = DialpadButtonBinding.inflate(LayoutInflater.from(context), this, true);
         title = binding.title;
         message = binding.message;
 
+        // Handles attributes from XML
         if (attrs != null) {
             TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.dialpad_button);
             try {
+                // Get color and size for the title
                 int titleColor = typedArray.getColor(R.styleable.dialpad_button_title_color, title.getCurrentTextColor());
                 float titleSize = typedArray.getDimension(R.styleable.dialpad_button_title_text_size, title.getTextSize());
-
+                // Get color and size for the message
                 int messageColor = typedArray.getColor(R.styleable.dialpad_button_message_color, message.getCurrentTextColor());
                 float messageSize = typedArray.getDimension(R.styleable.dialpad_button_message_text_size, message.getTextSize());
 
+                // Sets the color and size of the title and message
                 title.setTextColor(titleColor);
                 title.setTextSize(titleSize);
-
                 message.setTextColor(messageColor);
                 message.setTextSize(messageSize);
 
+                // Sets the text of the title and message
                 setTitle(typedArray.getString(R.styleable.dialpad_button_title));
                 setMessage(typedArray.getString(R.styleable.dialpad_button_message));
 
@@ -88,34 +68,36 @@ public class DialpadButton extends ConstraintLayout {
             }
         }
 
+        // Creates an ObjectAnimator used to scale the button
         ObjectAnimator scaleAnimator = ObjectAnimator.ofPropertyValuesHolder(
                 this,
                 PropertyValuesHolder.ofFloat(View.SCALE_X, SCALE_UP),
                 PropertyValuesHolder.ofFloat(View.SCALE_Y, SCALE_UP)
         );
 
+        // Sets initial settings for the ObjectAnimator
         scaleAnimator.setRepeatCount(1);
         scaleAnimator.setRepeatMode(ValueAnimator.REVERSE);
         scaleAnimator.setDuration(100);
         scaleAnimator.setInterpolator(new LinearInterpolator());
 
+        // Sets the onClickListener on the root which triggers the ObjectAnimator
         binding.getRoot().setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 scaleAnimator.start();
             }
         });
-
-
     }
 
-
+    // Sets the title text and makes sure that only the first character will show
     public void setTitle(String titleText) {
         if (titleText != null && !titleText.isEmpty()) {
             title.setText(titleText.substring(0, 1));
         }
     }
 
+    // Sets the message text and makes sure that only the first 3 characters will show
     public void setMessage(String messageText) {
         if (messageText != null && !messageText.isEmpty()) {
             message.setText(messageText.substring(0, Math.min(3, messageText.length())));
