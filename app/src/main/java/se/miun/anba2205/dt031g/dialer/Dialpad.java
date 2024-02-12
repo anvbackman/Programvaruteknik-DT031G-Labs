5caf0a49bec6fdeb9a252dade60fe25e1faeb471
Andreas Backman <anba2205@student.miun.se>
2024-02-12 15:21:45 +0100

    Task 4 call done

--- app/src/main/java/se/miun/anba2205/dt031g/dialer/Dialpad.java
+++ app/src/main/java/se/miun/anba2205/dt031g/dialer/Dialpad.java
@@ -1,11 +1,14 @@
 package se.miun.anba2205.dt031g.dialer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -74,12 +77,16 @@ public class Dialpad extends ConstraintLayout {
         dialpadButtonZero = findViewById(R.id.dialpad_button_0);
         dialpadButtonPound = findViewById(R.id.dialpad_button_pound);
 
+
         if (dialpadButtonOne != null) {
             Log.d("Dialpad", "Button One initialized");
         }
 
 
         setButtonOnClickListener();
+        setBackspaceListener();
+        setLongClickListener();
+        callNumber();
 
 
 
@@ -140,10 +147,55 @@ public class Dialpad extends ConstraintLayout {
 
     }
 
-    
+    private void setBackspaceListener() {
+        Button backspaceButton = findViewById(R.id.clear_button);
 
-    private void setClickListener() {
+        backspaceButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!inputText.isEmpty()) {
+                    inputText = inputText.substring(0, inputText.length() - 1);
+                    System.out.println("AAAAAAAAAAAAa");
+                    numberEntered.setText(inputText);
+                }
+            }
+        });
 
+
+    }
+
+    private void setLongClickListener() {
+        Button backspaceButton = findViewById(R.id.clear_button);
+
+        backspaceButton.setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                inputText = "";
+                numberEntered.setText(inputText);
+                return true;
+            }
+        });
+    }
+
+    private void callNumber() {
+        Button callButton = findViewById(R.id.call_button);
+
+        callButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!inputText.isEmpty()) {
+                    String encodedNumber = Uri.encode(inputText);
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel: " + encodedNumber));
+                    System.out.println("Call");
+                    getContext().startActivity(intent);
+                    System.out.println("Initiating call to: " + inputText);
+
+                }
+
+            }
+        });
     }
 
 
