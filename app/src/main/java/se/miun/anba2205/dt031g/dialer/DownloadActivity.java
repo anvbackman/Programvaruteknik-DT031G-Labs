109b33c5e977ea466519e348034d450ac3285baa
Andreas Backman <anba2205@student.miun.se>
2024-02-29 18:45:56 +0100

    Task 5 voices download

--- app/src/main/java/se/miun/anba2205/dt031g/dialer/DownloadActivity.java
+++ app/src/main/java/se/miun/anba2205/dt031g/dialer/DownloadActivity.java
@@ -28,15 +28,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Objects;
@@ -47,7 +50,13 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class DownloadActivity extends AppCompatActivity {
 
-    public static final String URL_LOCATION = "https://dt031g.programvaruteknik.nu/dialer/voices/";
+    //    public static final String URL_LOCATION = "https://dt031g.programvaruteknik.nu/dialer/voices/";
+//    public static final String DOWNLOAD_PATH = "/<app-specific-storage>/voices/";
+    private static String paths = "";
+
+
+
+
     private WebView webView;
     private ProgressBar progressBar;
     private ViewGroup progressBarLayout;
@@ -69,7 +78,9 @@ public class DownloadActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         progressBarLayout = findViewById(R.id.progress_bar_layout);
 
-
+        String url = getString(R.string.url_location);
+//        String path = getString(R.string.storage_path);
+        String path = getIntent().getStringExtra("storagePath");
 
 
 
@@ -77,116 +88,99 @@ public class DownloadActivity extends AppCompatActivity {
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
-                if (url.equalsIgnoreCase(URL_LOCATION)) {
-                    startActivity(new Intent(getApplicationContext(), DownloadActivity.class));
-                    System.out.println("NOW OVERRIDING");
-                    return true;
-
-
-                }
-                return false;
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String clickURL = request.getUrl().toString();
+                startDownload(clickURL);
+                System.out.println("URL is: " + url);
+                System.out.println("path is: " + path);
+                return true;
             }
         });
 
-        webView.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
-                System.out.println("NOW DOWNLOADING");
-//                shouldOverrideUrlLoading(webView, s);
-
-                download();
-
-            }
-        });
+//        webView.setDownloadListener(new DownloadListener() {
+//            @Override
+//            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
+//                System.out.println("NOW DOWNLOADING");
+////                shouldOverrideUrlLoading(webView, s);
+//
+//                download(url, path);
+//
+////                imageUploadAsyncTask = new ImageUploadAsyncTask();
+////                imageUploadAsyncTask.execute(image);
+//
+//            }
+//        });
 
 
     }
 
-    private void download() {
-//        progressBarLayout.setVisibility(View.VISIBLE);
-        animateView(progressBarLayout, View.VISIBLE);
+    private void setFilePath(String filePath) {
+        this.paths = filePath;
 
-        ExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
+    }
 
+    public String getFilePath() {
+        return paths;
+    }
+
+    private void startDownload(String url) {
+        int cutLastIndex = url.lastIndexOf("/");
+        String fileName = url.substring(cutLastIndex + 1);
+//        String downloadPath = (url + fileName);
+        setFilePath(url);
+//        if (!downloadPath.isEmpty()) {
+//            setFilePath(downloadPath);
+//        }
+        download(url, fileName);
+
+//        if (!downloadPath.isEmpty()) {
+//            return Util.unzip(downloadedFilePath);
+//        } else {
+//            return "";
+//        }
+    }
+
+    private void download(String urlPath, String fileName) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.execute(new Runnable() {
-
-            int count;
-
             @Override
             public void run() {
-
-                //Background work here
                 try {
-
-                    // put your url.this is sample url.
-                    URL url = new URL(URL_LOCATION);
-                    URLConnection connection = url.openConnection();
+                    URL url = new URL(urlPath);
+                    HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
                     connection.connect();
-                    System.out.println("URL CONNECTED");
 
+                    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        File destination = new File(getFilesDir(), fileName);
 
-                    int lenghtOfFile = connection.getContentLength();
+                        try (BufferedInputStream input = new BufferedInputStream(connection.getInputStream());
+                             FileOutputStream output = new FileOutputStream(destination)) {
+                            byte[] data = new byte[1024];
+                            int bytesRead;
 
-                    // download the file
-
-                    InputStream input = connection.getInputStream();
-
-                    //catalogfile is your destenition folder
-//                    OutputStream output = new FileOutputStream(connection.getOutputStream());
-//                    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-                    OutputStream out = new FileOutputStream(new File(getExternalFilesDir(null), "downloaded_file.extension"));
-
-
-                    byte data[] = new byte[1024];
-
-                    long total = 0;
-
-                    while ((count = input.read(data)) != -1) {
-                        total += count;
-                        // publishing the progress....
-
-
-                        publishProgress(Integer.valueOf("" + (int) ((total * 100) / lenghtOfFile)));
-                        System.out.println("PUBLISHING");
-                        Thread.sleep(20);
-
-//                        try {
-//                            Thread.sleep(3);
-//                        }
-//                        catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-
-
-                        // writing data to file
-                        out.write(data, 0, count);
-                    }
-
-                    // flushing output
-                    out.flush();
-
-                    // closing streams
-                    out.close();
-                    input.close();
-
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            //UI Thread work here
-                            animateView(progressBarLayout, View.GONE);
-                            System.out.println("PROGRESSBAR HIDDEN");
-
+                            while ((bytesRead = input.read(data)) != -1) {
+                                output.write(data, 0, bytesRead);
+                            }
                         }
-                    });
-                } catch (Exception e) {
 
+                        File destinationDir = new File(getFilesDir(), "voices");
+                        if (!destinationDir.exists()) {
+                            destinationDir.mkdir();
+                        }
+                        Util.unzip(destination, destinationDir);
+                        System.out.println("Downloaded file: " + destination);
+                        System.out.println("Destination: " + destinationDir);
+                    } else {
+                        throw new Exception("Failed to download file: " + connection.getResponseCode() + " " + connection.getResponseMessage());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
     }
+
+
 
     private void publishProgress(Integer... progress) {
 
@@ -194,6 +188,7 @@ public class DownloadActivity extends AppCompatActivity {
             @Override
             public void run() {
                 progressBar.setProgress(progress[progress.length - 1]);
+
             }
         });
 
@@ -214,49 +209,12 @@ public class DownloadActivity extends AppCompatActivity {
 
 
 
-//        URL url;
-//        HttpsURLConnection connection;
-//
-//        try {
-//            url = new URL(URL_LOCATION);
-//            connection = (HttpsURLConnection) url.openConnection();
-//
-//
-//            connection.setDoOutput(true);
-//            connection.setDoInput(true);
-//            connection.setUseCaches(false);
-//            connection.setRequestMethod("POST");
-//            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//
-//
-//            progressBar.setMax(20);
-//
-//
-//            connection.connect();
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        String response = "test";
-//
-//        return response;
-
-
-//    @Override
-//    protected void onProgressUpdate(Integer... values) {
-//        // Only publish the latest value
-//        progressBar.setProgress(values[values.length - 1]);
-//    }
-
-
-
 
     @Override
     protected void onResume() {
         super.onResume();
-        System.out.println("LOADED URL");
-        webView.loadUrl(URL_LOCATION);
+//        System.out.println("LOADED URL");
+        webView.loadUrl(getString(R.string.url_location));
 
     }
 
@@ -276,77 +234,6 @@ public class DownloadActivity extends AppCompatActivity {
     }
 
 
-
-//        webView.setDownloadListener(new DownloadListener());
-
-
-//
-//
-//
-//        webView.setWebViewClient(new WebViewClient());
-//
-//
-//        webView.setDownloadListener(new DownloadListener() {
-//            @Override
-//            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
-//                System.out.println("NOW DOWNLOADING");
-//
-//
-//
-//            }
-
-//        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                startActivity(new Intent(getApplicationContext(), ));
-//            }
-//        });
-
-
-
-
-//        webView.loadUrl(URL_LOCATION);
-//        webView.setDownloadListener(new DownloadListener() {
-//            @Override
-//            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
-//                System.out.println("NOW DOWNLOADING");
-//                Intent intent = new Intent(Intent.ACTION_VIEW);
-//                intent.setData(Uri.parse(url));
-//                startActivity(intent);
-//            }
-//        });
-
-
-//    private class DownloadListener implements android.webkit.DownloadListener {
-//
-//
-//        @Override
-//        public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-//            if (ContextCompat.checkSelfPermission(DownloadActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//                ActivityCompat.requestPermissions(DownloadActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-//
-//            }
-//            else {
-//                startDownload(url, contentDisposition, mimetype);
-//            }
-//        }
-//    }
-
-//    private void startDownload(String url, String contentDisposition, String mimetype) {
-//        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-//        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
-//        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-//        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-//        if (downloadManager != null) {
-//            long downloadId = downloadManager.enqueue(request);
-//        }
-
-
-//    private class DownloadAsyncTask extends AsyncTask<ImageProxy, Integer, String> {
-//
-//        private ProgressBar progressBar;
-//    }
 
 
 
